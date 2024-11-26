e4435c0c7c270b50ef9402a1f33f191b5cb0e9cd
Joe Moran <mojo@moranfoundation.org>
2024-11-25 21:41:17 -0800

    Use alternate DASH type 7 getStatus call for standalone getStatus sessions + Add new PodCommsSessions.getStatus parameter to specify noSeqGetStatus + Use new getStatus for post-connect, resuming setup and standalone getStatus + Update PodCommsSessions.send to handle alternative noSeqStatus msg # handling + Rename type 7 PodInfoRequestSubType to noSeqStatus for better clarity

--- OmniBLE/OmnipodCommon/MessageBlocks/PodInfo.swift
+++ OmniBLE/OmnipodCommon/MessageBlocks/PodInfo.swift
@@ -22,7 +22,7 @@ public enum PodInfoResponseSubType: UInt8, Equatable {
     case detailedStatus              = 0x02 // Returns detailed pod status, returned for most calls after a pod fault
     case pulseLogPlus                = 0x03 // Returns up to the last 60 pulse log entries plus additional info
     case activationTime              = 0x05 // Returns pod activation time and possible fault code & fault time
-    case noSeqStatusResponse         = 0x07 // DASH only, returns the normal status response w/o incrementing msg seq #
+    case noSeqStatus                 = 0x07 // DASH only, returns the normal status response w/o incrementing msg seq #
     case pulseLogRecent              = 0x50 // Returns the last 50 pulse log entries
     case pulseLogPrevious            = 0x51 // Like 0x50, but returns up to the previous 50 entries before the last 50
     
@@ -38,7 +38,7 @@ public enum PodInfoResponseSubType: UInt8, Equatable {
             return PodInfoPulseLogPlus.self
         case .activationTime:
             return PodInfoActivationTime.self
-        case .noSeqStatusResponse:
+        case .noSeqStatus:  // returns a normal StatusResponse, but using a seq # that not incremented from the last response
             return StatusResponse.self as! PodInfo.Type
         case .pulseLogRecent:
             return PodInfoPulseLogRecent.self
